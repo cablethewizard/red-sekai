43b193a63f710589bc51d42d3b22d57142b6bfa1
Cable <liam0611@gmail.com>
2024-11-08 10:35:45 -0500

    change commands to hybrid mode

--- regfox/regfox.py
+++ regfox/regfox.py
@@ -14,29 +14,29 @@ class RegFox(commands.Cog):
         self.config.register_guild(**default_guild)
         self.bot = bot
     
-    @commands.command()
+    @commands.hybrid_command(name="setregistrationlink")
     @commands.admin_or_permissions(manage_guild=True)
     async def setRegistrationLink(self,ctx,new_value):
         await self.config.guild(ctx.guild).registrationLink.set(new_value)
         await ctx.send("Registration link updated!")
     
-    @commands.command()
+    @commands.hybrid_command(name="setapikey")
     @commands.admin()
     async def setAPIKey(self,ctx,new_value):
         await self.config.guild(ctx.guild).apiKey.set(new_value)
         await ctx.send("RegFox API Key updated!")
 
-    @commands.command()
+    @commands.hybrid_command(name="setpageid")
     @commands.admin()
     async def setPageID(self,ctx,new_value):
         await self.config.guild(ctx.guild).pageid.set(new_value)
         await ctx.send("RegFox page ID updated!")
 
-    @commands.command()
+    @commands.hybrid_command(name="register")
     async def register(self,ctx):
         await ctx.send("Register for Sekaicon here: {}".format(self.config.guild(ctx.guild).registrationLink()))
 
-    @commands.command()
+    @commands.hybrid_command(name="regcount")
     async def regcount(self,ctx):
         response = requests.get(url="https://api.webconnex.com/v2/public/forms/{pageid}/inventory".format(pageid=self.config.guild(ctx.guild).pageid()), headers={"apiKey":"{key}".format(key=self.config.guild(ctx.guild).apiKey())})
         payload = response.json()
