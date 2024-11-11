3d8585b1e9b6d70f00d819925b68b8a6ef074d69
Cable <liam0611@gmail.com>
2024-11-11 13:45:15 -0500

    replace requests with aiohttp

--- regfox/regfox.py
+++ regfox/regfox.py
@@ -1,7 +1,7 @@
 from redbot.core import commands
 from redbot.core import Config
 from redbot.core.utils import chat_formatting
-import requests
+import aiohttp
 from fake_useragent import UserAgent
 
 class RegFox(commands.Cog):
@@ -15,32 +15,8 @@ class RegFox(commands.Cog):
         self.config.register_guild(**default_guild)
         self.bot = bot
         self.useragent = UserAgent()
-    """
-    async def regfoxRequest(self, ctx, url):
-        apikeyconf = await self.config.guild(ctx.guild).apiKey()
-        requrl = url
-        headers = {
-            "apiKey": "{}".format(apikeyconf),
-            "User-Agent": self.useragent.random
-        }
-        try:
-            response = requests.get(url=requrl, headers=headers)
-        except ConnectionError:
-            payload = "ERROR: Unable to reach WebConnex"
-            return payload
-        else:
-            if response.status_code == requests.codes.ok:
-                try:
-                    payload = response.json()
-                except requests.JSONDecodeError:
-                    payload = "JSON Decode Error"
-                    return payload
-                else:
-                    return payload
-            else:
-                payload = "ERROR {code}".format(code=response.status_code)
-                return
-    """
+        self.session = aiohttp.ClientSession('https://api.webconnex.com/v2/public')
+
     @commands.hybrid_command(name="setapikey")
     @commands.admin()
     async def setAPIKey(self,ctx,new_value):
@@ -57,26 +33,22 @@ class RegFox(commands.Cog):
     async def regcount(self,ctx):
         pageidconf = await self.config.guild(ctx.guild).pageid()
         apikeyconf = await self.config.guild(ctx.guild).apiKey()
-        url = "https://api.webconnex.com/v2/public/forms/{pageid}/inventory".format(pageid=pageidconf)
+        url = "/forms/{pageid}/inventory".format(pageid=pageidconf)
         headers = {
             "apiKey": "{}".format(apikeyconf),
             "User-Agent": self.useragent.random
         }
         try:
-            response = requests.get(url=url, headers=headers)
+            async with session.get(url, headers=headers) as response:
+                await apidata = response.json()
         except ConnectionError:
             await ctx.send("Connection error, unable to reach RegFox")
         else:
-            if response.status_code == requests.codes.ok:
-                try:
-                    payload = response.json()
-                except requests.JSONDecodeError:
-                    await ctx.send("JSON Decoding error")
-                else:
-                    await ctx.send("{count} currently registered!".format(count=payload['data'][1]['sold']))
+            if response.status == 200:
+                await ctx.send("{count} currently registered!".format(count=payload['data'][1]['sold']))
             else:
                 errorfile = chat_formatting.text_to_file(response.text,filename='error.txt',spoiler=False,encoding='utf-8')
-                await ctx.send("ERROR {code}".format(code=response.status_code), file=errorfile)
+                await ctx.send("ERROR {code}".format(code=response.status), file=errorfile)
     
     @commands.hybrid_command(name="connectiontest")
     @commands.admin()
@@ -88,14 +60,9 @@ class RegFox(commands.Cog):
             "User-Agent": self.useragent.random
         }
         try:
-            response = requests.get(url=url, headers=headers)
+            async with session.get(url, headers=headers) as response:
+                await apidata = response.json()
         except ConnectionError:
             await ctx.send("Connection error, unable to reach RegFox")
-        else:
-            try:
-                payload = response.json()
-            except requests.JSONDecodeError:
-                jsonerror = chat_formatting.text_to_file(response.text,filename='jsonerror.txt',spoiler=False,encoding='utf-8')
-                await ctx.send("JSON Decoding error", file=jsonerror)
-            else:
-                await ctx.send("Code {code}, MSG {data}".format(code=response.status_code, data=payload['data']))
+        else:             
+            await ctx.send("Code {code}, MSG {data}".format(code=response.status, data=apidata['data']))
